1f96a17aa58794dcdc7e2ce1d88e48f56741f39b
StepanTsymbal <tsymbalstepan@gmail.com>
2026-06-22 19:27:18 +0300

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          solver: ["cubejs"],
-          graphics: ["three", "@react-three/fiber"],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "");
+
+  return {
+    base: env.GITHUB_PAGES === "true" ? "/rubik-camera-solver/" : "/",
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            solver: ["cubejs"],
+            graphics: ["three", "@react-three/fiber"],
+          },
         },
       },
     },
-  },
+  };
 });
